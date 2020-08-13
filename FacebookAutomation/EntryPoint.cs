9cb65d8b5c1572c3c97689049dc715ffa682218c
muratbek.ishenbaev <matsuyun7@gmail.com>
2020-08-13 17:24:24 -0500

    Initial check-in of module FacebookAutomation

---
+++ FacebookAutomation/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using FacebookAutomation.Pages;
+using NUnit.Framework;
+using System;
+
+namespace FacebookAutomation
+{
+    public class EntryPoint
+    {
+        /*private static HomePage home;
+        public static void Main()
+        {
+            
+        }*/
+    }
+}
---
+++ FacebookAutomation/NavigateTo.cs
@@ -0,0 +1,14 @@
+﻿namespace FacebookAutomation
+{
+    using FacebookAutomation.Pages;
+
+    public static class NavigateTo
+    {
+        public static void SignIn()
+        {
+            LoginPage home = new LoginPage();
+            home.UsernameInput.SendKeys(Config.Credentials.Valid.Email);
+            
+        }
+    }
+}
